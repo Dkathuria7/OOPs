958d410b3a4a2d229403f5cbb2d8c9c72472872b
Dhairya Kathuria <91661759+Dkathuria7@users.noreply.github.com>
2022-04-12 09:48:28 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,21 @@
+
+public class Solution {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		if(args.length>0)
+		{
+		for(int i=0;i<args.length;i++)
+		//for(String data:args)
+		{
+				sum=sum+Integer.parseInt(args[i]);
+				
+		}
+		System.out.println(sum);
+		}
+		else
+		{
+			System.out.println("You did not pass any command line argument");
+		}
+	}
+}
